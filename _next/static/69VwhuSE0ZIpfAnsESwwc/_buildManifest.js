687ffadd422903d5ae21ba61add4ba3f00711098
Teo Jun Sheng, Pierre <junshengpierre@gmail.com>
2020-11-18 02:16:23 +0800

    chore(deploy): deploy to github pages

--- _next/static/69VwhuSE0ZIpfAnsESwwc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1bfc9850.82c39ba7e907caaf0dda.js","static\u002Fchunks\u002Fpages\u002Findex-2835cec1ff37a3d56717.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fb942535921b5be64a37.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
